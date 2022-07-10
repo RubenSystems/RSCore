b3c5f202962596e95fe0167aadb4d570fab34448
RubenSystems <ruben.t.j@icloud.com>
2022-07-10 09:51:30 +0100

    Added copy, move, copy assignment and move assignment to array to prevent odd behaviour

--- Array.h
+++ Array.h
@@ -1,70 +1,93 @@
-//
-//  Array.h
-//  RSwDE
-//
-//  Created by Ruben Ticehurst-James on 02/07/2022.
-//
+	//
+	//  Array.h
+	//  RSwDE
+	//
+	//  Created by Ruben Ticehurst-James on 02/07/2022.
+	//
 
-#ifndef Array_h
-#define Array_h
+	#ifndef Array_h
+	#define Array_h
 
-#include "Allocator.h"
-#include "Output.h"
+	#include "Allocator.h"
+	#include "Output.h"
 
-namespace core {
-	template <typename T>
-	class Array: public Allocator<T> {
-		public:
-			Array(typename Allocator<T>::index_type initial_size = 4) {
-				Allocator<T>::allocate(initial_size);
-			}
-		
-			~Array() {
-				if (Allocator<T>::data == nullptr)
-					return;
-				
-				for (int i = 0; i < currentSize; i ++) {
-					Allocator<T>::data[i].~T();
+	namespace core {
+		template <typename T>
+		class Array: public Allocator<T> {
+			public:
+				Array(typename Allocator<T>::index_type initial_size = 4) {
+					Allocator<T>::allocate(initial_size);
 				}
-			}
-		
-			void add (const T & item) {
-				if (currentSize >= Allocator<T>::allocated) {
-					Allocator<T>::allocate(Allocator<T>::allocated * enlargement);
-				}
-				Allocator<T>::operator[](currentSize ++) = item;
-			}
-		
-			void add (T && item) {
-				if (currentSize >= Allocator<T>::allocated) {
-					Allocator<T>::allocate(Allocator<T>::allocated * enlargement);
-				}
-				
-				// TODO: remove std lib 
-				Allocator<T>::operator[](currentSize ++) = std::move(item);
-			}
-		
-			// Construct and add (if you can come up with a better name email ruben.t.j@icloud.com)
-			template <typename... args>
-			void conadd(args && ... construction_parameters) {
-				if (currentSize >= Allocator<T>::allocated) {
-					Allocator<T>::allocate(Allocator<T>::allocated * enlargement);
-				}
-				
-				new(&(Allocator<T>::data[currentSize ++])) T(std::forward<T>(construction_parameters)...);
-			};
-		
-			typename Allocator<T>::index_type size() const {
-				return currentSize;
-			}
 			
+				Array(T type [], int size) {
+					
+					Allocator<T>::allocate(size);
+					memmove(Allocator<T>::data, type, sizeof(T) * size);
+				}
 			
-		
-		private:
-			static const int enlargement = 2;
-			typename Allocator<T>::index_type currentSize = 0;
-		
-	};
-}
+				~Array() {
+					if (Allocator<T>::data == nullptr)
+						return;
+					
+					for (int i = 0; i < currentSize; i ++) {
+						Allocator<T>::data[i].~T();
+					}
+				}
+			
+				Array(const Array & copy) : currentSize(copy.currentSize) {
+					
+				}
 
-#endif /* Array_h */
+				Array(Array && move) : currentSize(move.currentSize) {
+
+				}
+
+				Array & operator = (const Array & copy) {
+					currentSize = copy.currentSize;
+				}
+
+				Array & operator = (Array && move) {
+					currentSize = move.currentSize;
+				}
+
+				void add (const T & item) {
+					if (currentSize >= Allocator<T>::allocated) {
+						Allocator<T>::allocate(Allocator<T>::allocated * enlargement);
+					}
+					Allocator<T>::operator[](currentSize ++) = item;
+				}
+			
+				void add (T && item) {
+					if (currentSize >= Allocator<T>::allocated) {
+						Allocator<T>::allocate(Allocator<T>::allocated * enlargement);
+					}
+					
+					// TODO: remove std lib
+					Allocator<T>::operator[](currentSize ++) = std::move(item);
+				}
+			
+				// Construct and add (if you can come up with a better name email ruben.t.j@icloud.com)
+				template <typename... args>
+				void conadd(args && ... construction_parameters) {
+					if (currentSize >= Allocator<T>::allocated) {
+						Allocator<T>::allocate(Allocator<T>::allocated * enlargement);
+					}
+					
+					new(&(Allocator<T>::data[currentSize ++])) T(std::forward<T>(construction_parameters)...);
+				};
+			
+				typename Allocator<T>::index_type size() const {
+					return currentSize;
+				}
+				
+				
+			
+			private:
+				static const int enlargement = 2;
+				typename Allocator<T>::index_type currentSize = 0;
+			
+		};
+	}
+
+	#endif /* Array_h */
+
